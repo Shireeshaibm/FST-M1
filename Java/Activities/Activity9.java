d6ec3460e866125286c85adc1f7b2dfb5dccb066
Shireeshaibm <105856293+Shireeshaibm@users.noreply.github.com>
2024-09-16 17:45:03 +0530

    Update Activity_9.java

--- Java/Activities/Activity9.java
+++ Java/Activities/Activity9.java
@@ -1,27 +1,49 @@
-package Activities;
-
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Activity9 {
-    public static void main(String[] args) {
-        ArrayList<String> arrList = new ArrayList<String>();
-        arrList.add("Harry");
-        arrList.add("Nancy");
-        arrList.add("Carol");
-        arrList.add("Milan");
-        arrList.add("Ignis");  
+public class Activity_9 {
+	public static void main(String[] args) {
+		
 
-        for(String names: arrList){
-            System.out.println(names);
-        }
+	
+	
+	ArrayList<String> myList =new ArrayList<String>();
+	
+	myList.add("kid1");
+	myList.add("kid2");
+	myList.add("kid3");
+	myList.add("kid4");
+	myList.add("kid5");
+	
+	for(String kid:myList) {
+		
+		System.out.println(kid);
+	}
+	
+	System.out.println("third kid name is "+myList.get(2));
+	
+	Scanner s= new Scanner(System.in);
+	String kid = s.next();
+	
+	
+	System.out.println("checking if the "+kid+" is present");
+	
+	if(myList.contains(kid)){
+		
+	System.out.println("kid is present");
+	}
+	else {
+		System.out.println("kid is not present");
+	}
+	System.out.println("Number of kids in AL is "+myList.size());
+	
+	myList.remove(3);
+	
+	System.out.println("1 kid has been removed");
+	
+	System.out.println("Number of kids in AL is "+myList.size());
+	
 
-        System.out.println("Name in 3rd position: "+arrList.get(2));
-
-        System.out.println("Check name exists? "+arrList.contains("Carol"));
-
-        System.err.println("Size of list: "+arrList.size());
-
-        System.out.println("Remove name "+arrList.remove("Milan")+ " Size is now "+arrList.size());
-    }
-    
+}
+	
 }
