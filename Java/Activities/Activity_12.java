9a2086f82501f2881b7bd872528f4a9a14843806
Shireeshaibm <105856293+Shireeshaibm@users.noreply.github.com>
2024-09-16 19:35:07 +0530

    Update and rename Activity12.java to Activity_12.java

---
+++ Java/Activities/Activity_12.java
@@ -0,0 +1,16 @@
+
+
+
+public class Activity_12 {
+    public static void main(String[] args) {
+
+        Addable ad1 = (a, b) -> (a + b);
+        
+        Addable ad2 = (int a, int b) -> {
+            return (a + b);
+        };
+        
+
+        System.out.println(ad2.add(100, 200));
+    }
+}
